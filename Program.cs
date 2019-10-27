2b9b6f2e8040d066944e8f1af392a56acf6b9b00
observerjnr <rvangasyuk@gmail.com>
2019-10-27 16:20:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DictionaryImplementation
+{
+    class Program
+    {
+        class Dictionary<TKey, TValue>
+        {
+            int capacity = 1;
+            public void Remove( TKey key )
+            {
+
+            }
+
+            public void Add( TKey key, TValue value )
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
